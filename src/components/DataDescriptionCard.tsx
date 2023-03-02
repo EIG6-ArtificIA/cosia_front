f7b40e70bda8ef7aad5dd100f5c40267d1cba8df
maxi_malain <maxime.allain.8@gmail.com>
2023-03-02 20:07:08 +0100

    make it responsive

--- src/components/DataDescriptionCard.tsx
+++ src/components/DataDescriptionCard.tsx
@@ -1,4 +1,5 @@
 import { fr, FrCxArg } from "@codegouvfr/react-dsfr";
+import { Divider, Grid } from "@mui/material";
 import { makeStyles } from "tss-react/dsfr";
 
 const useStyles = makeStyles()((theme) => ({
@@ -26,7 +27,7 @@ const useStyles = makeStyles()((theme) => ({
   },
   title: {
     color: theme.decisions.text.title.blueFrance.default,
-    marginBottom: 0,
+    marginBottom: fr.spacing("1v"),
   },
   infoContainer: {
     marginBottom: 0,
@@ -37,61 +38,73 @@ const useStyles = makeStyles()((theme) => ({
   icon: {
     marginRight: fr.spacing("1w"),
   },
-  counters: {
-    "span + span": {
-      marginLeft: fr.spacing("3v"),
-    },
-  },
 }));
+
+type CounterT = { icon: FrCxArg; count: number; description: string };
+
+const counts: CounterT[] = [
+  {
+    icon: "fr-icon-eye-line",
+    count: 177,
+    description: "Vues",
+  },
+  { icon: "fr-icon-download-fill", count: 82, description: "Téléchargements" },
+  { icon: "fr-icon-send-plane-fill", count: 35, description: "Partages" },
+];
 
 export const DataDescriptionCard = () => {
   const { classes, cx } = useStyles();
   const dataName = "COSIA";
   const dataDescription = "COuverture du Sol par Intelligence Artificielle";
 
-  type CounterT = { icon: FrCxArg; count: number; description: string };
   const getCounter = ({ icon, count, description }: CounterT) => {
     return (
-      <span>
+      <Grid item xs={12} sm="auto">
         <span className={cx(fr.cx(icon, "fr-icon--xs"), classes.icon)} />
         {count} {description}
-      </span>
+      </Grid>
     );
   };
 
-  const counts: CounterT[] = [
-    {
-      icon: "fr-icon-eye-line",
-      count: 177,
-      description: "Vues",
-    },
-    { icon: "fr-icon-download-fill", count: 82, description: "Téléchargements" },
-    { icon: "fr-icon-send-plane-fill", count: 35, description: "Partages" },
-  ];
-
   return (
-    <div className={classes.container}>
-      <img src={require("../assets/img/carte_de_predictions_2.png")} className={classes.image} />
+    <Grid container className={classes.container}>
+      <Grid item xs={12} md="auto">
+        <img src={require("../assets/img/carte_de_predictions_2.png")} className={classes.image} />
+      </Grid>
 
-      <div>
-        <div className={classes.titleContainer}>
+      <Grid item xs container justifyContent="space-between" flexDirection="column">
+        <Grid item className={classes.titleContainer}>
           <h1 className={classes.title}>{dataName}</h1>
           <div>
             <b>{dataDescription}</b>
           </div>
-        </div>
+        </Grid>
 
-        <div className={classes.infoContainer}>
+        <Grid item className={classes.infoContainer}>
           <div className={classes.infoItem}>Producteur : IGN</div>
 
-          <div>
-            Création : <time dateTime="2022-08-04">04 août 2022</time> | Mise à jour :{" "}
-            <time dateTime="2023-03-02">02 mars 2022</time>
-          </div>
+          <Grid
+            container
+            columnSpacing={fr.spacing("3v")}
+            mb={fr.spacing("1v")}
+            rowSpacing={fr.spacing("1v")}
+          >
+            <Grid item xs={12} sm="auto">
+              Création : <time dateTime="2022-08-04">04 août 2022</time>
+            </Grid>
+            <Grid item xs={0} sm="auto" sx={{ display: { xs: "none", sm: "block" } }}>
+              <Divider orientation="vertical" />
+            </Grid>
+            <Grid item xs={12} sm="auto">
+              Mise à jour : <time dateTime="2023-03-02">02 mars 2022</time>
+            </Grid>
+          </Grid>
 
-          <div className={classes.counters}>{counts.map((count) => getCounter(count))}</div>
-        </div>
-      </div>
-    </div>
+          <Grid container columnSpacing={fr.spacing("3v")} rowSpacing={fr.spacing("1v")}>
+            {counts.map((count) => getCounter(count))}
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 };
