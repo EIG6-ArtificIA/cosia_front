cb42fe9eb04fcd08693e4570a52e1420142c9e3f
maxi_malain <maxime.allain.8@gmail.com>
2023-09-26 18:37:07 +0200

    clean

--- src/components/DownloadForm.tsx
+++ src/components/DownloadForm.tsx
@@ -1,10 +1,10 @@
 import { fr } from "@codegouvfr/react-dsfr";
-import { FormEvent, memo, useState } from "react";
-import { makeStyles } from "tss-react/dsfr";
+import Button from "@codegouvfr/react-dsfr/Button";
 import { Input } from "@codegouvfr/react-dsfr/Input";
 import { Select } from "@codegouvfr/react-dsfr/Select";
+import { FormEvent, memo, useState } from "react";
+import { makeStyles } from "tss-react/dsfr";
 import { DOWNLOADABLE_TERRITORIES } from "../data/downloadableTerritories";
-import Button from "@codegouvfr/react-dsfr/Button";
 
 const useStyles = makeStyles()({
   container: {
