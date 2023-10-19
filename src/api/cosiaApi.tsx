8b496d12e45c00cdb958222b42422d5b6a1ce08c
Maxime Allain <maxime.allain.8@gmail.com>
2023-10-19 18:53:20 +0200

    wip

--- src/api/cosiaApi.tsx
+++ src/api/cosiaApi.tsx
@@ -15,3 +15,30 @@ export type Department = {
 export const getAllDepartments = (): Promise<{ data: Department[] }> => {
   return cosiaApiAxiosInstance.get("departments");
 };
+
+const cosiaApiAxiosInstanceAvecHttps = axios.create({
+  baseURL: process.env.REACT_APP_API_URL_AVEC_HTTPS,
+  timeout: 1500,
+});
+
+export const getAllDepartmentsAvecHttps = (): Promise<{ data: Department[] }> => {
+  return cosiaApiAxiosInstanceAvecHttps.get("departments");
+};
+
+const cosiaApiAxiosInstanceSansCosiaAvecHttps = axios.create({
+  baseURL: process.env.REACT_APP_API_URL_SANS_COSIA_AVEC_HTTPS,
+  timeout: 1500,
+});
+
+export const getAllDepartmentsSansCosiaAvecHttps = (): Promise<{ data: Department[] }> => {
+  return cosiaApiAxiosInstanceSansCosiaAvecHttps.get("departments");
+};
+
+const cosiaApiAxiosInstanceSansCosia = axios.create({
+  baseURL: process.env.REACT_APP_API_URL_SANS_COSIA,
+  timeout: 1500,
+});
+
+export const getAllDepartmentsSansCosia = (): Promise<{ data: Department[] }> => {
+  return cosiaApiAxiosInstanceSansCosia.get("departments");
+};
--- src/pages/TestAPI.tsx
+++ src/pages/TestAPI.tsx
@@ -1,11 +1,23 @@
 import { useState } from "react";
 import Button from "@codegouvfr/react-dsfr/Button";
-import { Department, getAllDepartments } from "../api/cosiaApi";
+import {
+  Department,
+  getAllDepartments,
+  getAllDepartmentsAvecHttps,
+  getAllDepartmentsSansCosia,
+  getAllDepartmentsSansCosiaAvecHttps,
+} from "../api/cosiaApi";
 
 export const TestAPI = () => {
   const [depts, setDepts] = useState<Department[]>([]);
+  const [depts2, setDepts2] = useState<Department[]>([]);
+  const [depts3, setDepts3] = useState<Department[]>([]);
+  const [depts4, setDepts4] = useState<Department[]>([]);
 
   const fetchDepts = () => {
+    console.log("Chirac");
+    console.log(process.env.REACT_APP_API_URL);
+    console.log(process.env);
     getAllDepartments()
       .then(res => {
         console.log("Hollande");
@@ -15,12 +27,90 @@ export const TestAPI = () => {
       .catch(e => console.log(e));
   };
 
+  const fetchDepts2 = () => {
+    console.log(process.env.REACT_APP_API_URL);
+    getAllDepartmentsAvecHttps()
+      .then(res => {
+        console.log("Hollande");
+        console.log(res);
+        setDepts2(res.data);
+      })
+      .catch(e => console.log(e));
+  };
+
+  const fetchDepts3 = () => {
+    console.log(process.env.REACT_APP_API_URL);
+    getAllDepartmentsSansCosia()
+      .then(res => {
+        console.log("Hollande");
+        console.log(res);
+        setDepts3(res.data);
+      })
+      .catch(e => console.log(e));
+  };
+
+  const fetchDepts4 = () => {
+    console.log(process.env.REACT_APP_API_URL);
+    getAllDepartmentsSansCosiaAvecHttps()
+      .then(res => {
+        console.log("Hollande");
+        console.log(res);
+        setDepts4(res.data);
+      })
+      .catch(e => console.log(e));
+  };
+
   return (
     <section>
-      <Button onClick={fetchDepts}>C'est parti pour les départements !!</Button>
       <h1>Departements</h1>
+      <Button onClick={fetchDepts}>C'est parti pour les départements !!</Button>
       <ul>
         {depts.map(dep => {
+          return (
+            <li>
+              {dep.number} - {dep.name}
+              <ul>
+                <li>{dep.status}</li>
+                <li>{dep.geom}</li>
+              </ul>
+            </li>
+          );
+        })}
+      </ul>
+      <h1>Departements HTTPS</h1>
+      <Button onClick={fetchDepts2}>C'est parti pour les départements !! HTTPS</Button>
+      <ul>
+        {depts2.map(dep => {
+          return (
+            <li>
+              {dep.number} - {dep.name}
+              <ul>
+                <li>{dep.status}</li>
+                <li>{dep.geom}</li>
+              </ul>
+            </li>
+          );
+        })}
+      </ul>
+      <h1>Departements SANS COSIA</h1>
+      <Button onClick={fetchDepts3}>C'est parti pour les départements !! Bis</Button>
+      <ul>
+        {depts3.map(dep => {
+          return (
+            <li>
+              {dep.number} - {dep.name}
+              <ul>
+                <li>{dep.status}</li>
+                <li>{dep.geom}</li>
+              </ul>
+            </li>
+          );
+        })}
+      </ul>
+      <h1>Departements SANS COSIA AVEC HTTPS</h1>
+      <Button onClick={fetchDepts4}>C'est parti pour les départements !! Bis</Button>
+      <ul>
+        {depts4.map(dep => {
           return (
             <li>
               {dep.number} - {dep.name}
